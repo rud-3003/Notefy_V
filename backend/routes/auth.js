3937817a62f56f09e9e6b5dd2bdcfe335f0afc1d
Rudra Gupta <114010809+rud-3003@users.noreply.github.com>
2024-06-07 17:15:29 +0530

    Creating Login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ var jwt = require("jsonwebtoken");
 
 const JWT_SECRET = 'Thereoncewasaship$'
 
+//Route 1
 //Create a User using: POST "/api/auth/createuser". Doesn't require Auth
 router.post("/createuser", [
     body('name', 'Enter a valid Name').isLength({ min: 3 }),
@@ -22,30 +23,69 @@ router.post("/createuser", [
         //Check whether user with this email already exists
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ error: "A User with this emal already exists" });
+            return res.status(400).json({ error: "A User with this email already exists" });
+        }
+        else {
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
+            // Create a new user
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: secPass
+            });
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
+
+            // res.json(user)
+            res.json({ authToken: authToken });
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
+})
+
+//Route 2 
+// Authenticating a user using: POST "/api/auth/login" No Login required
+router.post("/login", [
+    body('email', 'Enter a valid Email Address').isEmail(),
+    body('password', 'Password cannot be blank').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Incorrect Username or Password" });
         }
 
-        const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password, salt);
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Incorrect Username or Password" });
+        }
 
-        // Create a new user
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: secPass
-        });
         const data = {
-            user:{
+            user: {
                 id: user.id
             }
         }
         const authToken = jwt.sign(data, JWT_SECRET);
 
         // res.json(user)
-        res.json({authToken: authToken});
+        res.json({ authToken: authToken });
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Some error occured");
+        res.status(500).send("Internal Server Error Occured");
     }
 })
 
