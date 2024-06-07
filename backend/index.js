4595cadb8c191477b07db498a9d0a9132f14454b
Rudra Gupta <114010809+rud-3003@users.noreply.github.com>
2024-06-07 12:12:55 +0530

    Adding Routes and Schema

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+//Available Routes
+app.use('/api/auth', require('./routes/auth.js'));
+app.use('/api/notes', require('./routes/notes.js'));
 
 app.listen(port, ()=>{
     console.log(`App listening at http://localhost:${port}`);
