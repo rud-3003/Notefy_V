561db229b79a568c045fa60dc2ba23db5747ef76
Rudra Gupta <114010809+rud-3003@users.noreply.github.com>
2024-06-10 18:09:27 +0530

    Route Setup and NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Notefy from "./Components/Notefy";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1> This is Notefy</h1>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Notefy/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Notefy.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Notefy() {
+  return (
+    <div>Welcome to Notefy!</div>
+  )
+}
